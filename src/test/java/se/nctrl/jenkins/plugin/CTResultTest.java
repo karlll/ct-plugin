564154ed05df5cf275cd0bb5af37d680210c2c1d
karlll <karl@ninjacontrol.com>
2012-11-05 16:26:25 +0100

    Added test for CTResult

---
+++ src/test/java/se/nctrl/jenkins/plugin/CTResultTest.java
@@ -0,0 +1,70 @@
+
+package se.nctrl.jenkins.plugin;
+
+import hudson.model.AbstractBuild;
+import hudson.tasks.test.TestObject;
+import hudson.tasks.test.TestResult;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author karl
+ */
+public class CTResultTest {
+    
+    
+    public CTResultTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of addChild method, of class CTResult.
+     */
+    @Test
+    public void testAddChild() {
+        
+        CTResult parent = new CTResult(null);
+        CTResult child = new CTResult(null);
+
+        parent.addChild(child);
+
+        assertEquals(parent, child.getParent());
+    
+    }
+
+    /**
+     * Test of findCorrespondingResult method, of class CTResult.
+     */
+    @Test
+    public void testFindCorrespondingResult() {
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getDisplayName method, of class CTResult.
+     */
+    @Test
+    public void testGetDisplayName() {
+        fail("The test case is a prototype.");
+    }
+}
